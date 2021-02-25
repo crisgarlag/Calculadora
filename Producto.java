c6ad3f3fa8723e6129b1c52651a04ac280d97a5a
bea87 <agui.14.87@gmail.com>
2021-02-25 15:40:15 +0100

    primer commit

---
+++ Producto.java
@@ -0,0 +1,40 @@
+
+
+/**
+ * Con esta clase podemos ejecutar todos los metodos relacionados con producto.
+ * @author beina 
+ * @version 1.0
+ * @since 21-02-2021
+ * 
+ */
+
+public class Producto {
+		
+		private int operandoEntero1;
+		private int operandoEntero2;
+		private double operandoReal1;
+		private double operandoReal2;
+		private double operandoReal3;
+		
+		private int solucionEntero;
+		private double solucionReal;
+		
+		private int valorAcumulado = 0;
+		
+		/**
+		 * Producto de dos numeros reales
+		 * @param operandoReal1 operando real, parametro de entrada,
+		 * @param operandoReal2 operando real, parametro de entrada,
+		 * @return solucionReal operando real, parametro de salida, solucion al producto de los dos parametros de entrada.
+		 */
+		
+		public double productoDosReales (double _operandoReal1, double _operandoReal2) {
+			double solucionReal = _operandoReal1 * _operandoReal2;
+			return solucionReal;
+		}
+	
+		
+		
+	}
+
+

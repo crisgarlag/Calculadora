227f766103a56031086f5d854b833e8e0a45afbf
AmselemSarah <78873263+AmselemSarah@users.noreply.github.com>
2021-03-01 16:16:22 +0100

    SegundoCommit

--- Resta.java
+++ Resta.java
@@ -23,14 +23,32 @@ public class Resta {
 	
 	private int acum = 0;
 	
+
+	
+	public Resta(int opEntero1, int opEntero2, int opEntero3, double opReal1, double opReal2, double opReal3,
+			int solEntero, double solReal, int acum) {
+		super();
+		this.opEntero1 = opEntero1;
+		this.opEntero2 = opEntero2;
+		this.opEntero3 = opEntero3;
+		this.opReal1 = opReal1;
+		this.opReal2 = opReal2;
+		this.opReal3 = opReal3;
+		this.solEntero = solEntero;
+		this.solReal = solReal;
+		this.acum = acum;
+	}
+
+
 	/**
 	 * Resta de dos números reales
 	 * @see Resta
 	 *@param  opReal1 y opReal2 son parámetros de entrada, reales y no negativo
 	 *@return solReal es la solución a la resta de los dos parámetros de entrada. Ha de ser real. 
 	 */
+
 	public double restaDosReales(double opReal1, double opReal2) {
-		this.solReal = opReal1 + opReal2;
+		this.solReal = opReal1 - opReal2;
 		return solReal;
 	}
 }

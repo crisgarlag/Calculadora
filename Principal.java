cfce60902c2d00b00d382f6d5e9a92d8452ee21e
AlbertoAguado <a.aguadochacon@gmail.com>
2021-03-01 12:15:44 +0100

    Añadidos metodos Suma a la clase Principal

--- Principal.java
+++ Principal.java
@@ -5,6 +5,12 @@ public class Principal {
 			Cociente coc1= new Cociente(10,5);
 			System.out.println(coc1.division(coc1.getNumInt1(), coc1.getNumInt2()));
 			
+			Suma test1 = new Suma(25, 75, 96, 45.85, 96.05, 68.25, 33, 62.07, 20);
+			System.out.println(test1.sumaDosReales(test1.getOperandoReal1(), test1.getOperandoReal2()));
+			System.out.println(test1.sumaDosEnteros(test1.getOperandoEntero1(), test1.getOperandoEntero2()));
+			System.out.println(test1.sumaTresReales(test1.getOperandoReal1(), test1.getOperandoReal2(), test1.getOperandoReal3()));
+			System.out.println(test1.sumaValorAcumulado(test1.getOperandoEntero1()));
+			
 
 	}
 

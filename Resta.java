04e1449e0ecb7a827d3cae91c9d9cca856c55500
AmselemSarah <78873263+AmselemSarah@users.noreply.github.com>
2021-03-01 17:38:23 +0100

    RestaFinal

--- Resta.java
+++ Resta.java
@@ -2,7 +2,7 @@
  * 
  * @author Sarah Amselem
  * @version 1.0
- * @since 21/02/21
+ * @since 01/03/21
  * @see Resta
  *
  *La clase contiene métodos para ejecutar operaciones de sustracción entre distintos parámetros. Esta clase forma parte de una 
@@ -23,7 +23,22 @@ public class Resta {
 	
 	private int acum = 0;
 	
-
+/**
+ * Constructor con todos los parámetros
+ * 
+ * 	@param opEntero1
+ *  @param opEntero2
+ *  @param opEntero3
+ *  @param opReal1
+ *  @param opReal2
+ *  @param opReal3
+ *  
+ *  @param solEntero
+ *  @param solReal
+ *  
+ *  @param acum
+ * 
+ */
 	
 	public Resta(int opEntero1, int opEntero2, int opEntero3, double opReal1, double opReal2, double opReal3,
 			int solEntero, double solReal, int acum) {
@@ -49,7 +64,11 @@ public class Resta {
 
 	public double restaDosReales(double opReal1, double opReal2) {
 		this.solReal = opReal1 - opReal2;
-		return solReal;
+		if (solReal < -999999999) {
+			return solReal;
+		} else {
+			return -999999999;
+		
 	}
 	/**
 	 * Resta de dos números enteros
@@ -59,7 +78,10 @@ public class Resta {
 	 */
 	public int restaDosEnteros(int opEntero1, int opEntero2) {
 		this.solEntero = opEntero1 - opEntero2;
-		return solEntero;
+		if (solEntero < -999999999) {
+			return solEntero;
+		} else {
+			return -999999999;
 	} 
 	/**
 	 * Resta de tres números reales
@@ -69,7 +91,10 @@ public class Resta {
 	 */
 	public double restaTresReales(double opReal1, double opReal2, double opReal3) {
 		this.solReal = opReal1 - opReal2 - opReal3;
-		return solReal;
+		if (solReal < 999999999) {
+			return solTresReales;
+		} else {
+			return -999999999;
 	}
 	/**
 	 * Resta con valor acumulado
@@ -78,7 +103,16 @@ public class Resta {
 	 */
 	public void restaValorAcumulado(int opEntero1) {
 		this.acum -= opEntero1;
+		if (this.valorAcumulado < -999999999) {
+			this.valorAcumulado = 999999999;
 	}
 	
-	
+	/*
+	 * Setters y getters
+	 */
+		
+		
+		
+		
+		
 }
